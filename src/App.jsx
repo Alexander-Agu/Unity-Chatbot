e057c0439ae3190d916d358ddff5768b212f211f
Alexander-Agu <theonlyagu@gmail.com>
2025-03-30 21:34:17 +0200

    intergrated the gemini API

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import { GoogleGenAI } from '@google/genai';
 import './App.css'
+import ReactMarkdown from 'react-markdown';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [userInput, setUserInput] = useState("");
+  const [count, setCount] = useState(1);
+  let prompt = "You are a chatbot specialized in the Unity game engine. Respond only with relevant information, advice, or solutions related to Unity development. If the user's question or statement is unrelated, politely redirect them by saying, 'I only answer Unity-related questions. plus return the headings as h1 and h2 in markdown' \n\nUser: " + userInput;
+  const [chatHistory, setChatHistory] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+
+  // let chatHis = ["Ask or comment anything about the Unity Game Engine"];
+
+
+
+  const GEMINI_API_KEY = import.meta.env.VITE_GEMINI_API_KEY;
+  const Gemini = new GoogleGenAI({apiKey: GEMINI_API_KEY});
+
+  const chat = async ()=> {
+    try{
+      const response = await Gemini.models.generateContent({
+        model: 'gemini-2.0-flash-001',
+        contents: prompt,
+      });
+
+      setCount(+1);
+      // chatHis.push(response.text);
+      setChatHistory(response.text)
+      console.log(response.text);
+    } catch {
+
+    } finally {
+
+    }
+  }
+
+  useEffect(()=>{
+
+  },[chatHistory])
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <header className='header'>
+      <h2>Unity</h2>
+    </header>
+
+    <main className='main'>
+        <article className='input'>
+          <input type="text"
+            onChange={e => setUserInput(e.target.value)}
+            value={userInput}
+          />
+
+          <button onClick={()=> chat()}>
+            Send
+          </button>
+        </article>
+
+        <article className='displayResults'>
+          {/* {
+            chatHis.map(x => {
+              
+              return <p>{x}</p>
+            })
+          } */}
+    
+          <ReactMarkdown>{chatHistory}</ReactMarkdown>
+        </article>
+    </main>
     </>
   )
 }
