6fc4fa20a4e9a346f1deb6ac5610dd08b2f1435a
Alexander-Agu <theonlyagu@gmail.com>
2025-04-05 10:50:26 +0200

    Styled the app component

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,10 @@ import ReactMarkdown from 'react-markdown';
 
 function App() {
   const [userInput, setUserInput] = useState("");
-  const [count, setCount] = useState(1);
   let prompt = "You are a chatbot specialized in the Unity game engine. Respond only with relevant information, advice, or solutions related to Unity development. If the user's question or statement is unrelated, politely redirect them by saying, 'I only answer Unity-related questions. plus return the headings as h1 and h2 in markdown' \n\nUser: " + userInput;
-  const [chatHistory, setChatHistory] = useState("");
+  const [chatResponse, setChatResponse] = useState("");
   const [isLoading, setIsLoading] = useState(true);
 
-  // let chatHis = ["Ask or comment anything about the Unity Game Engine"];
 
 
 
@@ -24,10 +22,7 @@ function App() {
         contents: prompt,
       });
 
-      setCount(+1);
-      // chatHis.push(response.text);
-      setChatHistory(response.text)
-      console.log(response.text);
+      setChatResponse(response.text)
     } catch {
 
     } finally {
@@ -35,39 +30,33 @@ function App() {
     }
   }
 
-  useEffect(()=>{
-
-  },[chatHistory])
-
 
   return (
     <>
-    <header className='header'>
-      <h2>Unity</h2>
-    </header>
-
     <main className='main'>
-        <article className='input'>
-          <input type="text"
-            onChange={e => setUserInput(e.target.value)}
-            value={userInput}
-          />
+      <article className='header'>
+        <h1>
+          UNITY GAME ENGINE
+        </h1>
+      </article>
 
-          <button onClick={()=> chat()}>
-            Send
-          </button>
-        </article>
 
-        <article className='displayResults'>
-          {/* {
-            chatHis.map(x => {
-              
-              return <p>{x}</p>
-            })
-          } */}
-    
-          <ReactMarkdown>{chatHistory}</ReactMarkdown>
-        </article>
+      <article className='responseContainer'>
+        <ReactMarkdown>{chatResponse}</ReactMarkdown>
+      </article>
+
+
+      <article className='inputContainer'>
+        <input type="text"
+          onChange={e => setUserInput(e.target.value)}
+          value={userInput}
+          placeholder='Ask about the Unity Game Engine.'
+        />
+
+        <button onClick={()=> chat()}>
+          Send
+        </button>
+      </article>
     </main>
     </>
   )
